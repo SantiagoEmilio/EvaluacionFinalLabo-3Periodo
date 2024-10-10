04ec95f1a929b2ce2896cc2e62a2805464f430af
Santiago <slopez@scl.edu.gt>
2024-10-10 11:49:10 -0600

    intento del trabajo del compo

---
+++ main.js
@@ -0,0 +1,16 @@
+import { navBarDiv } from "./COMPONENTES/componente_1/componente_1";
+
+
+let DOM = document.querySelector("#root");
+
+
+let navBar = document.createElement('section');
+navBar.className = "nav-bar";
+navBar.appendChild(navBarDiv);
+DOM.appendChild(navBar);
+
+
+let proyect = document.createElement('section');
+proyect.className = "proyect";
+proyect.appendChild (proyectos());
+DOM.appendChild(proyect);
